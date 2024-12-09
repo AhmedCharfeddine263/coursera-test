226174cb5d7e5feb7f437bcab06eabd01fae8989
Ahmed CHARFEDDINE <acharfeddine@sofrecom.fr>
2024-12-09 10:31:51 +0100

    next version of web file

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 (function () {
+    'use strict';
+    var x="hello";
     angular.module('myFirstApp',[])
-    .controller('MyFirstController', function(){
-        
+    .controller('MyFirstController', function($scope){
+        $scope.name="Ahmed";
+        $scope.sayhello = function(){
+            return "hello courserra";
+        };
     });
 })();
